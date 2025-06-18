1c448b7b42b86587aacde3520dbf51689914c492
hunghx <baby10051999@gmail.com>
2025-06-18 18:58:52 +0700

    feat new task

--- src/main/java/ra/ojt/BookingMassageApplication.java
+++ src/main/java/ra/ojt/BookingMassageApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BookingMassageApplication {
+    // code sau khi hoàn thành tassk
 
     public static void main(String[] args) {
         SpringApplication.run(BookingMassageApplication.class, args);
